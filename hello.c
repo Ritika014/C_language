0498b029143923c0b0f75da7afe767c199494ac3
Ritika014 <ritikabailwal99@gmail.com>
2021-01-01 14:44:42 +0530

    added comments

--- hello.c
+++ hello.c
@@ -1,9 +1,12 @@
 // This is first program in C.
-#include <stdio.h>           
+#include <stdio.h>          
+ /*Preprocessor command:t tells compiler to include stdio.h file before going to actual compilation*/
 
-int main()                   
+int main()    
+/*main function where execution begins */               
 {
     printf("Hello World!");    
+    /* printf() is a acother function which causes message Hello World! to display on the screen*/
     return 0;
-
+    /*return 0; terminates the main() function and returns the value 0*/
 }
